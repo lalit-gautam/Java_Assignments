be1b21910dd2abb543fcb57ea7922874ded70871
papu_sahoo <sahoopapu3450@gmail.com>
2023-03-12 15:07:34 +0530

    assignment

---
+++ Papu/Back-End/Assignment_18-12-mar/Assignement02.java
@@ -0,0 +1,12 @@
+//some of all elements in an array
+ublic class Assignement02 {
+    public static void main(String[] args) {
+        int[] arr={12,45,-1,58};
+        int sum=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            sum+=arr[i];
+        } 
+        System.out.println(sum);
+    }
+}
