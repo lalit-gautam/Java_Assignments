8bb9691cec735b9f789181cc8a3e994c6dddc19c
kritikub <kriti.behera@bipros.com>
2023-03-29 08:58:36 +0530

    Added Assignments for 29-03-2023

---
+++ Kriti/Backend/Assignment_29_03_2023/DailyRoutine.java
@@ -0,0 +1,10 @@
+package Assignment_23_03_2023;
+
+public interface DailyRoutine {
+     void morningStart() throws Exception;
+     void freshCheck() throws Exception;
+     void morningFood() throws Exception;
+
+     void travelToOffice() throws Exception;
+
+}
---
+++ Kriti/Backend/JavaBasics/BasicForLoopProgram.java
@@ -0,0 +1,7 @@
+package JavaBasics;
+
+public class BasicForLoopProgram {
+    public static void main(String[] args) {
+
+    }
+}
