4220e0b54d4f86f8a97d958ecd86d6ae16bc69d1
kritikub <kriti.behera@bipros.com>
2023-03-31 00:02:53 +0530

    AddedAssignment31-03-2023andAdded LookNSay program

--- Kriti/Backend/Assignment_30_03_2023/LookNSay.java
+++ Kriti/Backend/Assignment_30_03_2023/LookNSay.java
@@ -1,4 +1,4 @@
-package Assignment_23_03_2023;
+
 import java.util.Scanner;
 public class LookNSay {
     public static void main(String[] args) {
@@ -13,10 +13,14 @@ public class LookNSay {
         }
     }
     public static String printSequence(String num) {
-        String str = num;
+        String str = num ;
         String ans = "";
         int count = 1;
-        for (int i = 1; i < str.length() ; i++) {
+        for (int i = 1; i <= str.length() ; i++) {
+            if(i==str.length()){
+                ans = ans + count + str.charAt(i-1);
+                break;
+            }
             if (str.charAt(i-1) != str.charAt(i)) {
                 ans = ans + count + str.charAt(i-1);
                 count = 1;
